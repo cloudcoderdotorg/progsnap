14291151d5fb6a0b880875456941796af31b19b4
David Hovemeyer <david.hovemeyer@gmail.com>
2017-02-22 08:54:07 -0500

    setting up as a Python package

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# __init__.py for progsnap
+
+from progsnap import ProgsnapError, TagHandler, Assignment, Student, Position, Edit, Submission, Compilation, TestResults, WorkHistory, Dataset, TextDocument, EditApplicator
+
+__all__ = ['progsnap']
---
+++ setup.py
@@ -0,0 +1,9 @@
+setup(name='progsnap',
+      version='0.1.0',
+      description='Library for reading Progsnap datasets',
+      url='https://github.com/cloudcoderdotorg/progsnap',
+      author='David Hovemeyer',
+      author_email='david.hovemeyer@gmail.com',
+      license='MIT',
+      packages=['progsnap'],
+      zip_safe=False)
